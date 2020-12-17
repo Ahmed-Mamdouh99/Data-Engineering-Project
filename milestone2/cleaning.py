68a4cd2ad9b21ba9238bbe1eb3024cd586d41069
Ahmed Shams <ahmed.awadshams@student.guc.edu.eg>
2020-12-17 22:10:28 +0200

    Added Merge datasets

--- milestone2/cleaning.py
+++ milestone2/cleaning.py
@@ -1,5 +1,7 @@
 import pandas as pd
-
+import re
+import numpy as np
+import os
 def clean_wh(data2015,data2016,data2017,data2018,data2019)->pd.DataFrame:
   data2015.drop(['Happiness Rank', 'Standard Error'], axis=1, inplace=True)
   data2015['Year'] = 2015
@@ -67,4 +69,21 @@ def clean_250_cnt(df: pd.DataFrame)->pd.DataFrame:
   return countryData
 
 def merge_data(df_wrld_happi: pd.DataFrame, df_life_exp: pd.DataFrame, df_250_cnt: pd.DataFrame)->pd.DataFrame: # TODO: Implement this
-  pass
+  worldHappines = df_wrld_happi[df_wrld_happi['Year']==2015].copy()
+	worldHappines.drop('Year',axis=1,inplace=True)
+
+	df_life_exp.rename(columns = {"country":"Country", "year":"Year"}, inplace=True)
+	df_250_cnt.rename(columns = {"name":"Country"}, inplace=True)
+
+	before = ['Somaliland region', 'Bolivia (Plurinational State of)','Congo (Brazzaville)','Congo (Kinshasa)','Iran (Islamic Republic of)', "Lao People's Democratic Republic", "Democratic People's Republic of Korea", 'Republic of Moldova', 'Russian Federation', 'Somaliland Region', 'Syrian Arab Republic', 'United Kingdom of Great Britain and Northern Ireland', 'United States of America', 'Venezuela (Bolivarian Republic of)', 'Viet Nam', 'Czechia']
+	after = ['Somaliland Region', 'Bolivia', 'Congo', 'Democratic Republic of the Congo', 'Iran', "Laos", "Republic of Korea", 'Moldova', 'Russia', 'Somalia', 'Syria', 'United Kingdom', 'United States', 'Venezuela', 'Vietnam', 'Czech Republic']
+	worldHappines['Country'].replace(before, after,inplace=True)
+	df_life_exp['Country'].replace(before, after,inplace=True)
+	worldAndLife = worldHappines.merge(df_life_exp, on=['Country'])
+
+	before = ['Bolivia', 'Congo (Democratic Republic of the)', 'Iran (Islamic Republic of)', "Korea (Democratic People's Republic of)", 'Moldova (Republic of)', 'Russian Federation', 'Syrian Arab Republic', 'United Kingdom of Great Britain and Northern Ireland', 'United States of America', 'Venezuela (Bolivarian Republic of)', 'Viet Nam']
+	after = ['Bolivia (Plurinational State of)', 'Democratic Republic of the Congo', 'Iran', 'Korea (Republic of)', 'Moldova', 'Russia', 'Syria', 'United Kingdom', 'United States', 'Venezuela', 'Vietnam']
+	worldAndLife['Country'].replace(before, after,inplace=True)
+	df_250_cnt['Country'].replace(before, after,inplace=True)
+	allDataMerged = worldAndLife.merge(df_250_cnt, on='Country')
+	return allDataMerged
